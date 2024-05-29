1ec072e2ed14f65719a8fd882131c03481215b77
kollert-bzz <kollert@bzz.ch>
2024-05-29 14:19:25 +0200

    db user

--- bfriend-backend/database/database.sql
+++ bfriend-backend/database/database.sql
@@ -3,8 +3,7 @@ USE b_friend;
 
 CREATE TABLE user_data (
     id int AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(255) NOT NULL
 );
