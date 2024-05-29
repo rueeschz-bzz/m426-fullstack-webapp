9bc9fa6a072504a21f50978f16481149c4d88ebc
klenkb-bzz <klenkb@bzz.ch>
2024-05-29 15:18:47 +0200

    package locks

---
+++ bfriend-backend/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const session = require("express-session")
+
+const authentification = require("./controller/authController.js")
+
+const app = express()
+const port = 9000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+app.use("", authentification)
+
+app.listen(port, () => {
+    console.log("Bfriend is running on port:", port)
+})
