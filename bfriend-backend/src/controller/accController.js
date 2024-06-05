7f10456bbaba9077fab3bdb0efcd6931f0348dbb
Florin - Sebastian Serban <serbanf@bzz.ch>
2024-06-05 12:01:24 +0200

    update

--- bfriend-backend/src/controller/accController.js
+++ bfriend-backend/src/controller/accController.js
@@ -46,6 +46,27 @@ router.get("/api/users/:id", async (req, res) => {
   })
 });
 
+router.get("/api/my-data", async (req, res) => {
+  const client = new Client({
+    user: 'postgres.vpphyjfdeemfzziyoqoh',
+    host: 'aws-0-eu-central-1.pooler.supabase.com',
+    database: 'postgres',
+    password: '*mx5i-psSERVnZ)',
+    port: 5432,
+  });
+
+  client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  client.query(`SELECT * FROM user_data WHERE username = $1;`, [String(req.session.user)], (error, response) => {
+    console.log(error, response)
+    client.end()
+    res.json(response.rows).send()
+  })
+});
+
 router.put("/api/deactivate", async (req, res) => {
   const client = new Client({
     user: 'postgres.vpphyjfdeemfzziyoqoh',
