9c617ab74dc5200b53208dd8509474db7e40990d
Florin - Sebastian Serban <serbanf@bzz.ch>
2024-06-05 10:16:33 +0200

    accController.js update 2

--- bfriend-backend/src/controller/accController.js
+++ bfriend-backend/src/controller/accController.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Client } = require('pg');
-<<<<<<< HEAD
-=======
 const {response} = require("express");
->>>>>>> 95146fd8fb7a9b6d0fd4554cf47130427a5cc5e6
 
 router.get("/api/users", async (req, res) => {
   const client = new Client({
@@ -15,36 +12,6 @@ router.get("/api/users", async (req, res) => {
     port: 5432,
   });
 
-<<<<<<< HEAD
-  try {
-    await client.connect((err, res) => {
-      if (err) {
-        console.log("Connection problem ayy");
-      } else {
-        console.log("Connected!");
-        client.query('SELECT * FROM user_data;', (err, res) => {
-          if (err) {
-            console.log("Connection problem");
-          } else {
-            console.log(res);
-            res.send(res.rows);
-          }
-          client.end()
-        })
-      }
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error retrieving user data');
-
-  } finally {
-    await client.end();
-  }
-});
-
-module.exports = router;
-=======
   client.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -57,6 +24,4 @@ module.exports = router;
   })
 });
 
-
 module.exports = router;
->>>>>>> 95146fd8fb7a9b6d0fd4554cf47130427a5cc5e6
