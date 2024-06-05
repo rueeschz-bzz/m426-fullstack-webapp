82d8cf42433b698f3f210c9521b7098b70f680d8
klenkb-bzz <klenkb@bzz.ch>
2024-06-05 10:56:27 +0200

    added username to session and added more data to put in the database

--- bfriend-backend/src/controller/authController.js
+++ bfriend-backend/src/controller/authController.js
@@ -35,20 +35,23 @@ router.get("/api/login", (req, res) => {
     }
 })
 
-router.post("/api/login", async (req, res) => {
+router.post("/api/registration", async (req, res) => {
     if(req.session.auth !== "aut") {
+        const name = JSON.stringify(req.body.name)
+        const lastname = JSON.stringify(req.body.lastname)
+        const age = JSON.stringify(req.body.age)
         const username = JSON.stringify(req.body.username)
         const email = JSON.stringify(req.body.email)
         const password = JSON.stringify(SHA256(req.body.password).words)
         
-        console.log("Uname" + username, "Email" + email, "PW" + password)
-        
-        if (!username || !email || !password) {
+        if (!username || !email || !password || !name || !lastname || !age) {
             return res.status(400).send('Missing required fields');
           }
         
-        const query = "INSERT INTO user_data (username, email, password) VALUES ($1, $2, $3)";
-        const values = [username, email, password]
+        req.session.user = username
+        
+        const query = "INSERT INTO user_data (first_name, last_name, age, username, email, password) VALUES ($1, $2, $3, $4, $5, $6)";
+        const values = [name , lastname, age, username, email, password]
 
         try {
             await client.query(query, values);
