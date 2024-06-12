42ab024f3d83ba66f433a46ee5a5f9601ff977c8
Poisonlocket <loribono@gmx.ch>
2024-06-12 13:04:35 +0200

    resolved some package issues, added a repo route #4thememez

--- bfriend-frontend/app/login/login-form.tsx
+++ bfriend-frontend/app/login/login-form.tsx
@@ -2,6 +2,10 @@
 /*
 Stuff still broken, form submission not working
 */
+
+import express, {
+    Response
+} from "express";
 import { useRouter } from "next/navigation"
 import {useEffect, useState} from "react"
 
@@ -20,13 +24,13 @@ export default function DevLoginform () {
         const login= {username, password}
         console.log("submitted")
         try{
-        const res = fetch("localhost:9000/api/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(login)
+            const res: Response = fetch("http://localhost:9000/api/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(login)
 
 
-        })
+            })
 
             if (res.status === 202) {
                 router.refresh()
@@ -55,7 +59,7 @@ export default function DevLoginform () {
 
 
     return (
-        <form onSubmit={handleSubmit}  className={"h-[36rem] flex-none ml-10"}>
+        <form onSubmit={handleSubmit}  className={"h-[28rem] flex-none ml-10"}>
             <label className={"text-xl"}>Username</label><br />
             <input
                 required={true}
@@ -75,35 +79,35 @@ export default function DevLoginform () {
                 onChange={(e)=> setpassword(e.target.value)}
                 value={password}
                 type={"password"}
-                   className={
-                       "mt-2 input input-bordered caret-black input-secondary text-black w-full max-w-xs z-50"
-                   }
-                   disabled={false}
-                   placeholder={"Password"}
+                className={
+                    "mt-2 input input-bordered caret-black input-secondary text-black w-full max-w-xs z-50"
+                }
+                disabled={false}
+                placeholder={"Password"}
             ></input> <br />
             <div className={"mt-5 flex my-auto justify-items-center"}>
                 <button type={"submit"}
-                    disabled={isLoading}
+                        disabled={isLoading}
 
-                className="overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group active:scale-90 transition-transform"
-            >
+                        className="overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group active:scale-90 transition-transform"
+                >
                     {isLoading && <span>Good</span>}
                     {!isLoading && <span>Log in</span>}
 
-                <span
-                    className="absolute w-36 h-32 -top-8 -left-2 bg-sky-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right"
-                ></span>
-                <span
-                    className="absolute w-36 h-32 -top-8 -left-2 bg-sky-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"
-                ></span>
-                <span
-                    className="absolute w-36 h-32 -top-8 -left-2 bg-sky-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"
-                ></span>
-                <span
-                    className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
-                >Yes do it</span
-                >
-            </button></div>
+                    <span
+                        className="absolute w-36 h-32 -top-8 -left-2 bg-sky-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right"
+                    ></span>
+                    <span
+                        className="absolute w-36 h-32 -top-8 -left-2 bg-sky-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"
+                    ></span>
+                    <span
+                        className="absolute w-36 h-32 -top-8 -left-2 bg-sky-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"
+                    ></span>
+                    <span
+                        className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
+                    >Yes do it</span
+                    >
+                </button></div>
 
         </form>
     )
---
+++ bfriend-frontend/app/repo/page.tsx
@@ -0,0 +1,8 @@
+import {redirect} from "next/navigation";
+
+export default function repo(){
+    redirect("https://github.com/poisonlocket/m426-fullstack-webapp")
+    return(
+        <main></main>
+    )
+}
